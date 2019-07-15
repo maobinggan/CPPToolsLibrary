39d1737cf8df46da38008486403d6861226e7dd6
hy chen <413884010@qq.com>
2019-07-15 15:28:58 +0800

    增加Convert头文件：无符号数转为有符号数字符串

---
+++ C++工具类库/Convert.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+
+class Convert
+{
+public:
+
+	//************************************
+	// Method:     ToStr_Int32
+	// Description:转字符串：32位无符号数转为有符号数
+	// Parameter:  UINT32 uInt32 - 
+	// Parameter:  OUT char * resStr - 
+	// Returns:    void - 
+	//************************************
+	static void ToInt32Str(UINT32 uInt32, OUT char* resStr)
+	{
+		//得到有符号数的正负号部分。
+		char symbol32 = uInt32 & 0x80000000 ? '-' : '+';
+		//得到有符号数的数字部分：(负数)补码减一取反，去掉符号位。
+		INT32 int32 = (symbol32 == '-') ? ~(uInt32 - 1) & 0x7FFFFFFF : uInt32;
+		//字符串拼接
+		sprintf(resStr, "%c%08X", symbol32, int32);
+	}
+
+	//************************************
+	// Method:     ToStr_Int8
+	// Description:转字符串：8位无符号数转为有符号数
+	// Parameter:  UINT8 uInt8 - 
+	// Parameter:  OUT char * resStr - 
+	// Returns:    void - 
+	//************************************
+	static void ToInt8Str(UINT8 uInt8, OUT char* resStr)
+	{
+		//得到有符号数的正负号部分。
+		char symbol8 = uInt8 & 0x80 ? '-' : '+';
+		//得到有符号数的数字部分：(负数)补码减一取反，去掉符号位。
+		INT8 int8 = (symbol8 == '-') ? ~(uInt8 - 1) & 0x7F : uInt8;
+		//字符串拼接
+		sprintf(resStr, "%c%02X", symbol8, int8);
+	}
+};
--- C++工具类库/UnitTest.cpp
+++ C++工具类库/UnitTest.cpp
@@ -1,36 +1,57 @@
 #pragma once
 #include "iostream"
 #include "String.h"
-
+#include "Convert.h"
 
 /**  
-* 函 数 名: StringTest 
-* 说    明：测试String
+* 函 数 名: ConvertTest 
+* 说    明：测试Convert
 * 返 回 值: void 
 */
-void StringTest() 
+void ConvertTest()
+{
+	//测试：32位、8位 无符号数转为有符号数字符串
+	UINT32 uInt32 = 0x80000001;		//三十二位无符号数 8000 0001 -> 有符号数 -7FFF FFFF
+	UINT8 uInt8 = 0x81;				//八位无符号数 81 -> 有符号数 -7F
+	char uInt32Str[20] = { 0 };
+	char uInt8Str[20] = { 0 };
+	Convert::ToInt32Str(uInt32, uInt32Str);
+	Convert::ToInt8Str(uInt8, uInt8Str);
+	printf("signed int32 = %s  signed int8 =%s \n", uInt32Str, uInt8Str);
+
+}
+
+/**
+* 函 数 名: StringTest
+* 说    明：测试String
+* 返 回 值: void
+*/
+void StringTest()
 {
 	//调用普通构造函数(由字符常量)
-	String str = "ABCD"; 
+	String str = "ABCD";
 
 	//调用拷贝构造函数
-	String str2 = str; 
+	String str2 = str;
 
 	//调用赋值运算符(由字符常量)
-	str = "1234";		
+	str = "1234";
 
 	//先调用substring函数，得到返回的char*，随后调用普通构造函数(由字符常量)将char*传递给新的String对象
-	String subStr = str.SubString(2); 
+	String subStr = str.SubString(2);
 
 	//String转为char*打印
 	printf("%s \n", subStr.ToPChar());
 }
 
 
-void main() 
+void main()
 {
 	/*测试 String*/
 	StringTest();
+
+	/*测试 Convert*/
+	ConvertTest();
 
 	system("pause");
 }
